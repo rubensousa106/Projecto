4a356080fff37c815817898d886f7c426de5b502
Ruben <rubensousa106@gmail.com>
2020-02-20 20:58:22 +0000

    Alterações na Classe Pessoa

--- Projecto/src/Pessoa.java
+++ Projecto/src/Pessoa.java
@@ -1,4 +1,10 @@
 public class abstract Pessoa{
 
+            /*Clientes, Clientes VIP, GESTOR DE CLIENTE VÃO EXTENDER DESTA CLASSE PORQUE TODAS ESTAS CLASSES TEM EM
+            COMUM ESTES 3 ELEMENTOS :
+                -NOME
+                -TELEFONE
+                -EMAIL
 
+             */
         }
